529b5db7cfefbc9643c4ce0f85d425aafa958167
MohammedGoat <hamoudaff05@gmail.com>
2025-12-22 11:59:50 -0500

    Initial commit

---
+++ init-db.js
@@ -0,0 +1,5 @@
+db = db.getSiblingDB('mongotask');
+
+db.tasks.insert({ "title": "Docker Course" });
+db.tasks.insert({ "title": "Docker Training" });
+db.tasks.insert({ "title": "Jenkinz Course" });
